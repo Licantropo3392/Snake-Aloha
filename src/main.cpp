90e9d406c779cdf3eac2d27c465847c0357f1a0d
Licantropo3392 <licantropo3392@gmail.com>
2026-04-22 22:52:16 +0200

    Initial support for 3DS

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,19 @@ int main() {
     Aloha::init(800, 480, "Snake");
     
     while (!Aloha::shouldClose()) {
-        Aloha::beginDrawing();
-        Aloha::clearBackground(BLACK);
+        Aloha::beginDrawing({0, 0, 0, 255});
 
-        Aloha::drawRectangle(100, 100, 200, 150, RED);
-        Aloha::drawCircle(400, 300, 50, BLUE);
+        // Aloha::drawRectangle(100, 100, 200, 150, {255, 0, 0, 255});
+        // Aloha::drawCircle(400, 300, 50, {0, 0, 255, 255});
+
+        Aloha::drawRectangle(50, 50, 100, 75, {255, 0, 0, 255});
+        Aloha::drawCircle(200, 150, 25, {0, 0, 255, 255});
+
+        Aloha::drawText("Hello, Aloha!", 300, 200, 20, {255, 255, 255, 255});
 
         Aloha::endDrawing();
     }
 
+    Aloha::close();
     return 0;
 }
