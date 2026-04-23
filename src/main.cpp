ca0ff937922595b0abcb3417be7466661185432d
Licantropo3392 <licantropo3392@gmail.com>
2026-04-23 18:20:22 +0200

    Refactor main.cpp to utilize latest Aloha edits

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
-#include <platform.hpp>
+#include <Aloha.hpp>
 
 int main() {
     Aloha::init(800, 480, "Snake");
+
+    Aloha::Rectangle rect1(100, 100, 200, 150, {255, 0, 0, 255});
+    Aloha::Circle circle1(400, 300, 50, {0, 0, 255, 255});
+    Aloha::Text text1("Hello, Aloha!", 10, 10, 30, {255, 255, 255, 255});
     
     while (!Aloha::shouldClose()) {
         Aloha::beginDrawing({0, 0, 0, 255});
@@ -9,10 +13,9 @@ int main() {
         // Aloha::drawRectangle(100, 100, 200, 150, {255, 0, 0, 255});
         // Aloha::drawCircle(400, 300, 50, {0, 0, 255, 255});
 
-        Aloha::drawRectangle(50, 50, 100, 75, {255, 0, 0, 255});
-        Aloha::drawCircle(200, 150, 25, {0, 0, 255, 255});
-
-        Aloha::drawText("Hello, Aloha!", 300, 200, 20, {255, 255, 255, 255});
+        rect1.draw();
+        circle1.draw();
+        text1.draw();
 
         Aloha::endDrawing();
     }
