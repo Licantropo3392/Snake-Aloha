8b2ac602ec1632f6f833ca8988d3948bd3dbb5e5
Licantropo3392 <licantropo3392@gmail.com>
2026-05-06 21:17:47 +0200

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,39 @@
 #include <Aloha.hpp>
 
-int main() {
-    // Aloha::init(800, 480, "Snake");
-    Aloha::init(400, 240, "Snake");
+int main()
+{
+    Aloha::Window mainWindow(800, 480, 60, "Snake", 0, true);
 
-    // Aloha::Rectangle rect1(100, 100, 200, 150, {255, 0, 0, 255});
-    // Aloha::Circle circle1(400, 300, 50, {0, 0, 255, 255});
-    // Aloha::Text text1("Hello, Aloha!", 10, 10, 30, {255, 255, 255, 255});
-    Aloha::Rectangle rect1(50, 50, 100, 75, {255, 0, 0, 255});
-    Aloha::Circle circle1(200, 150, 25, {0, 0, 255, 255});
-    Aloha::Text text1("Hello, Aloha!", 5, 5, 15, {255, 255, 255, 255});
-    
-    while (!Aloha::shouldClose()) {
-        Aloha::beginDrawing({0, 0, 0, 255});
+    Aloha::Rectangle rect1(mainWindow.getWidth() / 8, mainWindow.getWidth() / 8, mainWindow.getWidth() / 4, mainWindow.getHeight() / 3.2, {255, 0, 0, 255});
+    Aloha::Circle circle1(mainWindow.getWidth() / 2, mainWindow.getHeight() / 1.6, mainWindow.getWidth() / 16, {0, 0, 255, 255});
+    Aloha::Text text1("Hello, Aloha!", mainWindow.getWidth() / 80, mainWindow.getWidth() / 80, mainWindow.getHeight() / 16, {255, 255, 255, 255});
 
-        if (Aloha::isKeyDown(Aloha::KEYBOARD_SPACE) || Aloha::isGamepadButtonDown(Aloha::GAMEPAD_BUTTON_RIGHT_FACE_RIGHT)) {
-            text1.setText("Button pressed!");
-        } else {
-            text1.setText("Hello, Aloha!");
+    while (!mainWindow.shouldClose())
+    {
+        mainWindow.beginDrawing({0, 0, 0, 255});
+
+        for (int i = 0; i < 20; i++) {
+
+        }
+
+        if (Aloha::isKeyDown(Aloha::KEYBOARD_SPACE) || Aloha::isGamepadButtonDown(Aloha::GAMEPAD_BUTTON_RIGHT_FACE_RIGHT))
+        {
+            text1.setText("Changed to second screen!");
+            mainWindow.setTargetScreen(1);
+        }
+        if (Aloha::isKeyDown(Aloha::KEYBOARD_ENTER) || Aloha::isGamepadButtonDown(Aloha::GAMEPAD_BUTTON_RIGHT_FACE_DOWN))
+        {
+            text1.setText("Changed to first screen!");
+            mainWindow.setTargetScreen(0);
         }
 
         rect1.draw();
         circle1.draw();
         text1.draw();
 
-        Aloha::endDrawing();
+        mainWindow.endDrawing();
     }
 
-    Aloha::close();
+    mainWindow.close();
     return 0;
 }
